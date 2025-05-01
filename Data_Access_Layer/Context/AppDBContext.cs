94d143a04c64b020517390f83029ef027cb18290
AliBackSlash <ae1378032@gmail.com>
2025-05-01 16:46:56 +0300

    I made the basic files that we will launch in building the project

---
+++ Data_Access_Layer/Context/AppDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Data_Access.Context
+{
+    public class AppDBContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            //base.OnConfiguring(options);
+
+            var ConStr = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("ConnectionStrings").Value;
+
+            options.UseSqlServer(ConStr);
+        }
+    }
+}
