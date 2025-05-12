d1d74332edf1ab0b48fcba42b9440cd685f0299d
Ahmad <ahmadalsahory@yahoo.com>
2025-05-13 01:14:53 +0300

    Add UserDto properties and necessary namespaces
    This commit introduces several `using` directives in `UserDto.cs` to include essential namespaces. Additionally, new properties have been added to the `UserDto` class: `UserId`, `UserName`, `Permissions`, `IsActive`, and `PersonId`, enhancing the representation of user-related data.

--- Data_Access_Layer/DTOs/User_DTOs/UserDto.cs
+++ Data_Access_Layer/DTOs/User_DTOs/UserDto.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Data_Access.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,10 @@ namespace Data_Access.DTOs.User_DTOs
 {
     internal class UserDto
     {
+        public int UserId { get; set; }
+        public string UserName { get; set; } = null!;
+        public int Permissions { get; set; }
+        public int IsActive { get; set; }
+        public int PersonId { get; set; }
     }
 }
